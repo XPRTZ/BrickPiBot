6248d2b3e58864ff49e6b32be9aceee4679735d0
Tilborg, Stefan van <Stefan.van.Tilborg@ordina.nl>
2020-02-15 17:58:02 +0100

    Add project files.

---
+++ BrickPiTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Iot.Device.BrickPi3;
+using Iot.Device.BrickPi3.Movement;
+using System;
+using System.Threading.Tasks;
+
+namespace BrickPiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Brick brick = new Brick();
+            Motor motor = new Motor(brick, Iot.Device.BrickPi3.Models.BrickPortMotor.PortB);
+
+            motor.SetSpeed(10);
+            motor.SetTachoCount(0);
+            motor.Start();
+            await Task.Delay(500);
+            motor.Stop();
+
+            Console.WriteLine("Stop...");
+        }
+    }
+}
